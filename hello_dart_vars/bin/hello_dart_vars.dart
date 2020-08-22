f6ef2a4c197e88cd1a17a2539368466873226b0e
darren <darren@penguin>
2020-08-21 20:44:42 -0700

    basic edits

--- hello_dart_vars/bin/hello_dart_vars.dart
+++ hello_dart_vars/bin/hello_dart_vars.dart
@@ -1,4 +1,23 @@
 void main(List<String> arguments) {
   // entry point
+
+  /* Types:
+    strings
+    integers
+    doubles
+    booleans
+    lists
+    maps
+    ...
+  */
+
+  var country = 'Mozambique'; // string
+  var number = 12;
+  var doubleNum = 12.45;
+  var negNum = -14;
+  print(number);
+  print(doubleNum);
+  print(negNum);
   print('Hello World!');
+  print(country);
 }
