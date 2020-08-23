fc4b272486647b2be4ccc4d63d3937d47c2ac5e8
Darren Couch <kg7koi@outlook.com>
2020-08-23 07:27:19 -0700

    practising inheritance

--- intro_to_classes/bin/intro_to_classes.dart
+++ intro_to_classes/bin/intro_to_classes.dart
@@ -1,5 +1,37 @@
-import 'package:intro_to_classes/intro_to_classes.dart' as intro_to_classes;
+class Microphone {
+  String name;
+  String color;
+  int model;
+
+  Microphone(this.name, this.color, this.model);
+
+  Microphone.initialize() {
+    name = 'Blue Yeti';
+    model = 9823;
+  }
+
+  String turnOn() => '$name is on!';
+  String turnOff() => '$name is off!';
+}
+
+class Person {
+  String name, lastName, nationality;
+  int age;
+
+  String showName() => name;
+}
+
+class Bonnie extends Person {
+  String profession;
+
+  String showProfession() => profession;
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${intro_to_classes.calculate()}!');
+  var bonnie = Bonnie();
+  bonnie.name = 'Bonnie';
+  bonnie.age = 43;
+  bonnie.profession = 'Programmer';
+  print(bonnie.showProfession());
+  print(bonnie.showName());
 }
