1ca92ebb2d152a5c2ee6c3e1c1433fe4e9f9e1f9
Darren Couch <kg7koi@outlook.com>
2020-08-23 12:21:27 -0700

    initial commit

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/ui/home.dart';
+
+void main() => runApp(Home());
---
+++ hello_world/lib/ui/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.deepOrange,
+        child: Center(
+          child: Text("WOWZERS!!!",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 24,
+                fontStyle: FontStyle.italic,
+              )),
+        ));
+  }
+}
