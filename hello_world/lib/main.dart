941f57cf0a3c87f6c11f393e6ae220de5b0647dd
Darren Couch <kg7koi@outlook.com>
2020-08-24 10:40:46 -0700

    FInal save on flutter first class

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/ui/home.dart';
 
-void main() => runApp(Home());
+//void main() => runApp(ScaffoldExample());
+
+void main() => runApp(MaterialApp(
+      home: ScaffoldExample(),
+    ));
--- hello_world/lib/ui/home.dart
+++ hello_world/lib/ui/home.dart
@@ -1,5 +1,115 @@
 import 'package:flutter/material.dart';
 
+class ScaffoldExample extends StatelessWidget {
+  _tapButton() {
+    debugPrint('Tapped Button');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scaffold'),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent.shade100,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.ac_unit),
+              onPressed: () => debugPrint('AC UNIT TAPPED')),
+          IconButton(icon: Icon(Icons.access_alarms), onPressed: _tapButton),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.lightGreenAccent,
+          onPressed: () => debugPrint('FLOATING ACTION BUTTON'),
+          child: Icon(Icons.missed_video_call)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.add_alert), label: 'HOWDY'),
+          BottomNavigationBarItem(icon: Icon(Icons.alarm), label: 'FUUUCK')
+        ],
+        onTap: (int index) => debugPrint('Tapped item: $index'),
+      ),
+      backgroundColor: Colors.redAccent.shade100,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              onTap: _tapButton,
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.purpleAccent,
+                    border: new Border.all(color: Colors.white, width: 1.5),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Center(
+                    child: Text('CLICK ME',
+                        style: TextStyle(fontSize: 18, color: Colors.white))),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              onTap: _tapButton,
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    border: new Border.all(color: Colors.white, width: 1.5),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Center(
+                    child: Text('CLICK ME TOO',
+                        style: TextStyle(fontSize: 18, color: Colors.white))),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              onTap: _tapButton,
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.yellowAccent,
+                    border: new Border.all(color: Colors.white, width: 1.5),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Center(
+                    child: Text('CLICK ME THREE',
+                        style: TextStyle(fontSize: 18, color: Colors.black38))),
+              ),
+            ),
+            CustomButton()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          final snackBar = SnackBar(
+            content: Text('MAMMA MIA'),
+            backgroundColor: Colors.tealAccent,
+          );
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              color: Colors.pinkAccent,
+              borderRadius: BorderRadius.circular(8.0)),
+          child: Text('Button'),
+        ));
+  }
+}
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
